a0ea6664103ec43772bfaa6bb313100ec44b8250
João Victor <joaovictorvvm@edu.unifor.br>
2024-10-04 21:43:51 -0300

    substring

---
+++ Basic/substring.rb
@@ -0,0 +1,12 @@
+def substrings (text, dict)
+  res = {}
+  dict.each do |word|
+    qtd = (text.length - text.gsub(word,"").length) / word.length
+    if qtd > 0
+      res[word] = qtd
+    end
+  end
+  res
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
